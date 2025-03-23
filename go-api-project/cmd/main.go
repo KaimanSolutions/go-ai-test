b6590135a610b38c487f81e6a7b7bfb90b21ca05
KaimanSolutions <connorsj26@gmail.com>
2025-03-23 12:50:48 +0000

    second commit

--- go-api-project/cmd/main.go
+++ go-api-project/cmd/main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "go-api-project/internal/routes"
+	"go-api-project/internal/routes"
+	"log"
+	"net/http"
 )
 
 func main() {
-    mux := http.NewServeMux()
-    routes.SetupRoutes(mux)
+	mux := http.NewServeMux()
+	routes.SetupRoutes()
 
-    server := &http.Server{
-        Addr:    ":8080",
-        Handler: mux,
-    }
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
 
-    log.Println("Starting server on :8080")
-    if err := server.ListenAndServe(); err != nil {
-        log.Fatalf("Could not start server: %s\n", err)
-    }
+	log.Println("Starting server on :8080")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Could not start server: %s\n", err)
+	}
 }
--- go-api-project/go.mod
+++ go-api-project/go.mod
@@ -1,7 +1,3 @@
 module go-api-project
 
 go 1.18
-
-require (
-    github.com/swaggest/openapi-go v0.0.0-20220315123456-abcdef123456
-)
